a937d7b7f6c28800d3f67fd8e557e501898d8a1a
infinitywebau <team@infinityweb.com.au>
2026-05-13 10:27:03 +0300

    Update school-handbook.php

--- school-handbook.php
+++ school-handbook.php
@@ -27,14 +27,14 @@ function school_handbook_enqueue_assets() {
 			'school-handbook',
 			plugin_dir_url(__FILE__) . 'assets/css/handbook.css',
 			[],
-			'2.5'
+			'2.7'
 		);
 
 		wp_enqueue_script(
 			'school-handbook',
 			plugin_dir_url(__FILE__) . 'assets/js/handbook.js',
 			['jquery'],
-			'2.3',
+			'2.7',
 			true
 		);
 
